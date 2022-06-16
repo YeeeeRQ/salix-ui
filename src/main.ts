356ddf81395d8b8c6f8a6b7902f8471683fff68a
Wox <YeeeeRQ@outlook.com>
2022-06-16 15:56:46 +0800

    项目初始化，添加路由

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import Home from "./views/Home.vue";
+import Doc from "./views/Doc.vue";
+
+import { createWebHashHistory, createRouter } from "vue-router";
+
+const history = createWebHashHistory();
+const router = createRouter({
+  history: history,
+  routes: [
+    {
+      path: "/",
+      component: Home,
+    },
+    {
+      path: "/xxx",
+      component: Doc,
+    },
+  ],
+});
+
+const app = createApp(App)
+app.use(router);
+app.mount("#app");
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+
+<template>
+<div>
+    <div class="topNav">
+        <div class="logo"></div>
+        <div class="menu"></div>
+    </div>
+
+    <div class="banner">
+        <h1>Mint UI</h1>
+        <h2>简洁 轻量 </h2>
+        <h2>快速引用无负担</h2>
+        <p class="actions">
+            <a href="">GitHub</a>
+            <a href="">开始</a>
+        </p>
+    </div>
+</div>
+</template>

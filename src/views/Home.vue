f5bd3c1dfccacbe33100a2e7aa90f731a9079c2d
Wox <YeeeeRQ@outlook.com>
2022-07-12 23:57:39 +0800

    不同尺寸屏幕适配

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <TopNav />
   <div class="banner">
     <SvgIcon name="LOGO"></SvgIcon>
-    <h1>Salix</h1>
+    <h1>Salix UI</h1>
     <p class="tagline">简洁 轻量</p>
     <p class="tagline">快速引用无负担</p>
     <p class="actions">
@@ -10,11 +10,27 @@
       <router-link to="/doc" as="a">开始</router-link>
     </p>
   </div>
+  <div class="features">
+    <div class="featuresWrapper">
+      <article>
+        <h1>基于Vue3</h1>
+        <p>基于最新的 Composition API</p>
+      </article>
+      <article>
+        <h1>TypeScript</h1>
+        <p>组件源码采用 TypeScirpt 书写</p>
+      </article>
+      <article>
+        <h1>代码易读</h1>
+        <p>源码简洁，方便使用</p>
+      </article>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import TopNav from "../components/TopNav.vue";
-import SvgIcon from '../components/SvgIcon.vue';
+import SvgIcon from "../components/SvgIcon.vue";
 
 export default {
   name: "Home",
@@ -33,15 +49,7 @@ export default {
 }
 
 .topNavAndBanner {
-  // border: 1px solid red;
   overflow: hidden;
-  // background-color: #e2f0ec;
-  // background: linear-gradient(
-  //   135deg,
-  //   rgba(146, 224, 182, 1) 0%,
-  //   rgba(208, 251, 212, 1) 51%,
-  //   rgba(226, 240, 236, 1) 100%
-  // );
 }
 
 .banner {
@@ -55,7 +63,8 @@ export default {
   > h1 {
     font-size: 3.2rem;
     font-weight: lighter;
-    font-family: "lucida grande", "lucida sans unicode", lucida, helvetica, "Hiragino Sans GB", "Microsoft YaHei", "WenQuanYi Micro Hei", sans-serif;
+    font-family: "lucida grande", "lucida sans unicode", lucida, helvetica,
+      "Hiragino Sans GB", "Microsoft YaHei", "WenQuanYi Micro Hei", sans-serif;
   }
 
   > .tagline {
@@ -77,6 +86,39 @@ export default {
       padding: 0.6em 1em;
       margin: 0.3em;
       margin-top: 2.25rem;
+    }
+  }
+}
+
+.features {
+  width: 80%;
+  margin: 0 auto;
+
+  margin-bottom: 300px;
+
+  > .featuresWrapper {
+    display: flex;
+    flex-wrap: nowrap;
+    border-top: 1px solid rgba(0, 0, 0, 0.1);
+    border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+
+    @media (max-width: 800px) {
+      flex-direction: column;
+    }
+
+    > article {
+      flex: 1;
+      display: inline-block;
+      padding: 4rem 3rem;
+
+      @media (max-width: 800px) {
+        padding-top: 1.5rem;
+        padding-bottom: 1.5rem;
+        text-align: center;
+      }
+      > p {
+        padding-top: 1rem;
+      }
     }
   }
 }

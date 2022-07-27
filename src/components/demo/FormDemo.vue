8dbb36af839d779b06ed477f9ae1bc5471036292
Wox <YeeeeRQ@outlook.com>
2022-07-27 14:40:27 +0800

    FormItem功能添加：可通过Prop属性或自定义的标签设定表单Label

--- src/components/demo/FormDemo.vue
+++ src/components/demo/FormDemo.vue
@@ -2,10 +2,22 @@
   <h1>Form 示例</h1>
 
   <h2>基本</h2>
-  <sx-form-item>
-    <sx-input v-model="msg"></sx-input>
+  <sx-form-item label="账号：">
+    <sx-input v-model="msg" placeholder="用户名"></sx-input>
   </sx-form-item>
-  {{msg}}
+
+  <h2>通过Prop属性指定label</h2>
+  <sx-form-item label="UserName : ">
+    <sx-input v-model="msg" ></sx-input>
+  </sx-form-item>
+
+  <h2>使用自定义label</h2>
+  <sx-form-item >
+    <template #label>
+        <b>UserName: </b>
+    </template>
+    <sx-input v-model="msg" ></sx-input>
+  </sx-form-item>
 
 </template>
 
--- src/lib/Form/FormItem.tsx
+++ src/lib/Form/FormItem.tsx
@@ -1,15 +1,29 @@
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import './index.scss';
 
 export default defineComponent({
   name: "SxFormItem",
+  props:{
+    label:{
+      type: String,
+      default:""
+    }
+  },
   setup(props, { attrs, slots, emit }) {
+    const errMsg = ref('');
+
+    // label可通过props属性传入或使用自定义的标签传入
+    const renderLabel = ()=>{
+      return slots.label ? slots.label() : <label class="item-label">{props.label}</label>
+    }
+
     return () => {
       return (
         <div class="sx-form-item">
+          {renderLabel()}
           <div class="item-content">
             <div class="item-control-wrap">{slots.default!()}</div>
-            <p class="item-error"></p>
+            <p class="item-error" v-show="errMsg" >{errMsg.value}</p>
           </div>
         </div>
       );

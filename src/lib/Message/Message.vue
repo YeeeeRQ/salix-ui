556950f51bf022d5a5b6fa4df3190c321debaa7c
Wox <YeeeeRQ@outlook.com>
2022-08-08 00:12:34 +0800

    完善Message组件

--- src/lib/Message/Message.vue
+++ src/lib/Message/Message.vue
@@ -1,37 +1,44 @@
 <template>
-    <div class="sx-msg" :class="classes" v-if="visible">
+<transition
+    name="sx-msg-fade"
+    @before-leave="onClose"
+    @after-leave="emit('destroy', props.id)"
+>
+    <div class="sx-msg" :class="classes" v-show="visible" :style="styles">
         <!-- messagemessagemessagemessagemessagemessagemessagemessagemessagemessage message message message message -->
-        Hello World!
-        <span class="closeIcon"></span>
+        {{id}} {{message}}
+        <span class="closeIcon" @click="hideMessage"></span>
     </div>
+</transition>
 </template>
 
 <script lang="ts" setup>
 import './index.scss';
 import { IType } from './type';
 
-import {
-    computed,
-    ref,
-    onMounted,
-    onUnmounted,
-} from "vue";
+import { computed, ref, onMounted, onUnmounted} from "vue";
 
 interface Props {
-    id: string,
+    id: number,
     message?: string
     center?: boolean
     duration?: number
     type?: IType
+    offset?:number
+    onClose: ()=>void
 }
 
 const props = withDefaults(defineProps<Props>(), {
-    id: '',
     message: '',
     center: false,
-    duration: 3000,
+    duration: -1,
     type: 'info',
+    offset: 20,
 })
+
+const emit = defineEmits<{
+    (e:'destroy', id:number):void
+}>()
 
 const classes = computed(() => [
     "sx-msg-" + props.type, props.center ? "is-center" : "",
@@ -39,22 +46,34 @@ const classes = computed(() => [
 
 const visible = ref(true);
 
-const hideOnTime = (duration) => {
-    if (duration) {
-        let timer: any;
-        const startTimer = () => {
-            timer = setTimeout(() => {
-                visible.value = false;
-            }, duration);
-        };
-        onMounted(() => {
-            startTimer();
-            visible.value = true;
-        });
-        onUnmounted(() => {
-            clearTimeout(timer);
-        });
-    }
+const hideMessage = () => {
+    visible.value = false;
+    // emit('destroy', props.id);
 }
-hideOnTime(props.duration);
+
+const hideOnTime = () => {
+    let timer: any;
+    const startTimer = () => {
+        timer = setTimeout(() => {
+            hideMessage();
+        }, props.duration);
+    };
+    onMounted(() => {
+        startTimer();
+        visible.value = true;
+    });
+    onUnmounted(() => {
+        clearTimeout(timer);
+    });
+}
+
+// 不指定duration则需手动关闭message
+if (props.duration > 0) {
+    hideOnTime();
+}
+
+const styles = computed(()=>({
+    top:`${props.offset}px`,
+}))
+
 </script>
--- src/lib/Message/index.tsx
+++ src/lib/Message/index.tsx
@@ -1,28 +1,101 @@
 import Message from "./Message.vue";
-import { createApp } from "vue";
+import { createApp, h, render, VNode } from "vue";
+
+let id = 0;
+
+interface InstanceType {
+  container: HTMLDivElement;
+  vnode: VNode;
+  options: any;
+}
+const instances: InstanceType[] = [];
 
 export const openMessage = (options) => {
   const { content } = options;
 
-  createMessage();
+  const gap = 20;
+  let offset = options.offset || gap;
+  instances.forEach((i) => {
+    // console.log(vm);
+    offset += i.vnode.el!.offsetHeight + gap;
+  });
+
+  // 函数劫持
+  let userClose = options.onClose;
+  let opts = {
+    ...options,
+    offset,
+    id: id++,
+    onClose: () => {
+      // console.log('closed');
+
+      userClose?.();
+    },
+  };
+  createMsg(opts);
 };
 
-
-const createMessage = ()=>{
+const createMsg = (options) => {
   const container = document.createElement("div");
-  document.body.appendChild(container);
-  const app = createApp({
-    render() {
-      return (
-      <Message 
-        // center={false}
-        // duration={3000}
-        type={"success"}
-      >
-        hello
-      </Message>
-      )
-    },
-  });
-  app.mount(container);
-}
+
+  // const vnode = h(Message, options);
+  const vnode = h(Message, options);
+  console.log('create options:', options);
+
+  // 强制挂载onDestroy函数
+  vnode.props!.onDestroy = (delID: number) => {
+    console.log('del:', delID);
+    render(null, container);
+
+    let move = 0;
+    if((delID + 1) <= instances.length - 1){
+      move = instances[delID+1].options.offset - instances[delID].options.offset;
+    }
+    // console.log('move: ', move);
+
+    for(let i = delID+1; i<instances.length;i++){
+      // console.log('update: ', i);
+
+      const {container, options} = instances[i]
+      // console.log('options new:',options);
+
+      let newOptions = { ...options };
+      newOptions.offset -= move;
+      newOptions.id--;
+      // console.log('options:',newOptions);
+
+      const newVNode = h(Message, newOptions);
+      render(newVNode, container);
+      // console.log('render', i);
+      instances[i].options = newOptions;
+    }
+
+    instances.splice(delID, 1);
+    id--;
+    if (instances.length === 0)  id = 0;
+  };
+
+  render(vnode, container);
+  document.body.appendChild(container.firstElementChild!);
+
+  instances.push({ container, vnode, options });
+};
+
+// const createMessage = ()=>{
+//   const container = document.createElement("div");
+//   document.body.appendChild(container);
+//   const app = createApp({
+//     render() {
+//       return (
+//       <Message
+//         // center={false}
+//         // duration={3000}
+//         type={"success"}
+//       >
+//         hello
+//       </Message>
+//       )
+//     },
+//   });
+//   app.mount(container);
+// }

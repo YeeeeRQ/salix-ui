64a932fea11798ec56c0e95e927fb83b05f9498d
Wox <YeeeeRQ@outlook.com>
2022-08-09 14:32:25 +0800

    重构Tabs组件

--- src/lib/Tabs/Tabs.vue
+++ src/lib/Tabs/Tabs.vue
@@ -1,141 +1,100 @@
 <template>
   <div class="sx-tabs">
     <div class="sx-tabs-nav" ref="container">
-      <div v-for="(t, index) in titles" class="sx-tabs-nav-item" :class="{
-        selected: t.title === selectedTitle,
-        'sx-tabs-nav-disabled': t.disabled,
-      }" :key="index" :ref="
-  (el) => {
-    if (t.title === selectedTitle) selectedItem = el;
-  }
-" @click="select(t)">
+
+      <div v-for="(t, index) in titles" :key="index" @click="select(t)" class="sx-tabs-nav-item"
+        :ref="(el) => { if (t.title === selectedTitle) selectedItem = (el as any); }"
+        :class="{ selected: t.title === selectedTitle, 'sx-tabs-nav-disabled': t.disabled, }">
         {{ t.title }}
       </div>
+
       <div class="sx-tabs-nav-indicator" ref="indicator"></div>
+
     </div>
+
     <div class="sx-tabs-content">
-      <component class="sx-tabs-content-item" :is="current" :key="current.props.title" />
+      <component class="sx-tabs-content-item" :is="currentContent" :key="currentContent.props.title" />
     </div>
+
   </div>
 </template>
 
-<script lang="ts">
-import { computed, ref, onMounted, watchEffect } from "vue";
+<script lang="ts" setup>
+import './index.scss';
+import { computed, ref, onMounted, watchEffect, defineComponent, useSlots } from "vue";
 import Tab from "./Tab.vue";
-export default {
-  props: {
-    selectedTitle: {
-      type: String,
+
+const props = defineProps<{
+  selectedTitle: string,
+}>();
+
+const emit = defineEmits<{
+  (e: 'update:selectedTitle', title: string): void;
+}>()
+
+const slots = useSlots()
+
+// 动态计算indicator宽度
+const selectedItem = ref<HTMLDivElement | null>(null);
+const indicator = ref<HTMLDivElement | null>(null);
+const container = ref<HTMLDivElement | null>(null);
+onMounted(() => {
+  watchEffect(
+    () => {
+      if (selectedItem.value && container.value && indicator.value) {
+        const { width, left: resultLeft } =
+          selectedItem.value.getBoundingClientRect();
+        const { left: containerLeft } =
+          container.value.getBoundingClientRect();
+
+        indicator.value.style.width = width + "px";
+        indicator.value.style.left = resultLeft - containerLeft + "px";
+      }
     },
-  },
-  setup(props, context) {
-    const selectedItem = ref<HTMLDivElement | null>(null);
-    const indicator = ref<HTMLDivElement | null>(null);
-    const container = ref<HTMLDivElement | null>(null);
-    onMounted(() => {
-      watchEffect(
-        () => {
-          if(selectedItem.value && container.value && indicator.value){
-            const { width, left: resultLeft } =
-              selectedItem.value.getBoundingClientRect();
-            const { left: containerLeft } =
-              container.value.getBoundingClientRect();
+    {
+      flush: "post",
+    }
+  );
+});
 
-            indicator.value.style.width = width + "px";
-            indicator.value.style.left = resultLeft - containerLeft + "px";
-          }
-        },
-        {
-          flush: "post",
-        }
-      );
-    });
+// 子组件类型校验
+if (!slots.default) {
+  throw new Error("Tabs 子标签必须包含Tab");
+}
 
-    const defaults = context.slots.default();
-    defaults.forEach((tab) => {
-      if (tab.type !== Tab) {
-        throw new Error("Tabs 子标签必须为 Tab");
-      }
-    });
-    const titles = defaults.map((tab) => {
-      if (tab.props.disabled || tab.props.disabled === "") {
-        return {
-          title: tab.props.title,
-          disabled: true,
-        };
-      } else {
-        return {
-          title: tab.props.title,
-          disabled: false,
-        };
-      }
-    });
-    const select = (t: { title: string; disabled: boolean }) => {
-      if (!t.disabled) {
-        context.emit("update:selectedTitle", t.title);
-      }
-    };
-    const current = computed(() => {
-      return defaults.find((tab) => tab.props.title === props.selectedTitle);
-    });
+const defaultSlots = slots.default!();
+defaultSlots.forEach((tab) => {
+  if (tab.type !== Tab) {
+    throw new Error("Tabs 子标签必须为 Tab");
+  }
+});
 
-    return {
-      defaults,
-      titles,
-      current,
-      select,
-      selectedItem,
-      indicator,
-      container,
-    };
-  },
+// Tabs titles 
+const titles = defaultSlots.map((tab) => {
+  const disabled: boolean = tab.props!.disabled || tab.props!.disabled === "";
+  const title = tab.props!.title;
+  return {
+    title,
+    disabled,
+  }
+});
+
+const select = (t: { title: string; disabled: boolean }) => {
+  if (!t.disabled) {
+    emit("update:selectedTitle", t.title);
+  }
 };
+
+// Tabs content
+const currentContent = computed(() => {
+  return defaultSlots.find((tab) => tab.props!.title === props.selectedTitle);
+});
+
 </script>
 
-<style lang="scss">
-$blue: #40a9ff;
-$color: #333;
-$border-color: #d9d9d9;
-
-.sx-tabs {
-  &-nav {
-    display: flex;
-    color: $color;
-    border-bottom: 1px solid $border-color;
-    position: relative;
-
-    &-item {
-      padding: 8px 0;
-      margin: 0 16px;
-      cursor: pointer;
-
-      &:first-child {
-        margin-left: 0;
-      }
-
-      &.selected {
-        color: $blue;
-      }
-
-      &.sx-tabs-nav-disabled {
-        color: #ccc;
-        cursor: not-allowed;
-      }
-    }
-
-    &-indicator {
-      position: absolute;
-      height: 3px;
-      background: $blue;
-      left: 0;
-      bottom: -1px;
-      width: 100px;
-      transition: left 250ms, width 250ms;
-    }
-  }
-
-  &-content {
-    padding: 8px 0;
-  }
-}
-</style>
+<script lang="ts">
+export default defineComponent({
+  name: 'Tabs',
+  // inheritAttrs: false,
+})
+</script>
